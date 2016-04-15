5c7af4b7a0a5d2f4d6283ceab66281ab854993e3
yair <yaia1309@gmail.com>
2016-04-16 00:19:49 +0300

    added monte carlo to manual batch

--- resizeData.py
+++ resizeData.py
@@ -6,18 +6,18 @@ import os
 # Set path of data files
 path = "imgs"
 
-if not os.path.exists(path + "/trainResized64"):
-    os.makedirs(path + "/trainResized64")
-if not os.path.exists(path + "/testResized64"):
-    os.makedirs(path + "/testResized64")
+if not os.path.exists(path + "/trainResized40"):
+    os.makedirs(path + "/trainResized40")
+if not os.path.exists(path + "/testResized40"):
+    os.makedirs(path + "/testResized40")
 
-img_size_y = 48
-img_size_x = 64
+img_size_y = 40
+img_size_x = 40
 
 train_files_unlabeled = sorted(glob.glob(path + "/train/*"))
 for fol in train_files_unlabeled:
-    if not os.path.exists(path + "/trainResized64/" + fol.split("/")[-1]):
-        os.makedirs(path + "/trainResized64/" + fol.split("/")[-1])
+    if not os.path.exists(path + "/trainResized40/" + fol.split("/")[-1]):
+        os.makedirs(path + "/trainResized40/" + fol.split("/")[-1])
 
 train_files_labeled = []
 for fol in train_files_unlabeled:
@@ -27,12 +27,12 @@ for fol in train_files_labeled:
     for im_name in fol:
         image = imread(im_name)
         imageResized = resize(image, (img_size_y, img_size_x))
-        newName = "/".join(im_name.split("/")[:-2]) + "Resized64/" + "/".join(im_name.split("/")[-2:])
+        newName = "/".join(im_name.split("/")[:-2]) + "Resized40/" + "/".join(im_name.split("/")[-2:])
         imsave(newName, imageResized)
 
 testFiles = sorted(glob.glob(path + "/test/*"))
 for i, nameFile in enumerate(testFiles):
     image = imread(nameFile)
     imageResized = resize(image, (img_size_y, img_size_x))
-    newName = "/".join(nameFile.split("/")[:-1]) + "Resized64/" + nameFile.split("/")[-1]
+    newName = "/".join(nameFile.split("/")[:-1]) + "Resized40/" + nameFile.split("/")[-1]
     imsave(newName, imageResized)
